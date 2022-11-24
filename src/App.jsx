2eb1d6fbf495047cacae49e6b694c9bb06955802
AdeyemiAdekunle <bamidek@gmail.com>
2022-11-24 17:50:54 +0100

    update file

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>UNDER CONSTRUCTION</h1>
+      <div>
+        <img src="../src/image/under_construction.png" alt="underconstruction" />
+      </div>
+     
+    </div>
+  )
+}
+
+export default App
