927e82ee2a060304467fd14eebfcc531a7b23e27
AdeyemiAdekunle <bamidek@gmail.com>
2022-11-25 08:27:24 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
 import './App.css'
+import under_construction from './assets/under_construction.png'
 
 function App() {
-
 
   return (
     <div className="App">
       <h1>UNDER CONSTRUCTION</h1>
-      <div>
-        <img src="../src/image/under_construction.png" alt="underconstruction" />
+      <div className='image_container'>
+        <img src={under_construction}  alt="underconstruction" />
       </div>
      
     </div>
